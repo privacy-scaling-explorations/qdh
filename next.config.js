4ec0724722f965048da042430d7a2b94a096c953
Raman Shalupau <raman.shalupau@gmail.com>
2021-02-03 00:43:46 +0800

    updating docs and config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = withBundleAnalyzer({
     return [
       {
         source: `/api/qdh-general-config`,
-        destination: `https://strapi-admin.quadratic.page/qdh-general-config`,
+        destination: `${process.env.NEXT_PUBLIC_STRAPI_URL}/qdh-general-config`,
       },
       {
         source: `/api/tally/:filename`,
